fdbda9bcee0e0705680ab766ad9cb47d8f571eab
Alejandro <alejandrocalderonvelez@gmail.com>
2019-07-20 17:55:21 -0500

    Initial structure

---
+++ src/main/java/com/acalderon/demoBack/UserRepo.java
@@ -0,0 +1,7 @@
+package com.acalderon.demoBack;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Long> {}

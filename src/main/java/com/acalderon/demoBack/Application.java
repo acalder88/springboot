ac63766cb9e5099acc78f2eba91e25f3b1a45542
Alejandro Calderon Velez <acalderon@psl.com.co>
2021-04-13 06:11:18 -0500

    Added real MDB connection

--- src/main/java/com/acalderon/demoBack/Application.java
+++ src/main/java/com/acalderon/demoBack/Application.java
@@ -9,19 +9,7 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(UserRepo userRepository) {
-		return args -> {
-			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
-				User user = new User(name);
-				userRepository.save(user);
-			});
-			userRepository.findAll().forEach(System.out::println);
-		};
 	}
 }
